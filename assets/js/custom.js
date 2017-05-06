92b4ab26044509abac765e994604e9239689d10b
ab-hi <bansal.abhishek101@yahoo.com>
2017-05-06 12:47:38 +0530

    further Ui updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,4 +2,55 @@ $(document).ready(function(){
 	     $('.carousel').carousel();
 	     $('.carousel.carousel1').carousel({indicators: true});
 
+/* for animations on page scroll*/
+
+	     (function($) {
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+var win = $(window);
+
+var cards = $(".slide-in");
+
+
+
+cards.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  cards.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("slide_in"); 
+    } 
+
+  });
+  
+});
+
+ $(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000, 'swing');
+	});
+
 });
